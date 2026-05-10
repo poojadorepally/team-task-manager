cff463eecedcb09d0ff54df38b60c30bdd5f12ea
pooja <poojadorepally@gmail.com>
2026-05-10 22:46:16 +0530

    fixed port

--- app.py
+++ app.py
@@ -120,5 +120,6 @@ def tasks():
 with app.app_context():
     db.create_all()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
